44c6083ba0afe982af560147b18db32ee0213589
tcclevenger <tccleve@sandia.gov>
2021-12-02 14:50:21 -0700

    add

--- components/scream/src/physics/p3/atmosphere_microphysics_run.cpp
+++ components/scream/src/physics/p3/atmosphere_microphysics_run.cpp
@@ -4,8 +4,6 @@ namespace scream {
 
 void P3Microphysics::run_impl (const int dt)
 {
-  timer_all.start_timer();
-
   // Assign values to local arrays used by P3, these are now stored in p3_loc.
   Kokkos::parallel_for(
     "p3_main_local_vals",
@@ -19,20 +17,17 @@ void P3Microphysics::run_impl (const int dt)
   infrastructure.dt = dt;
   infrastructure.it++;
 
-  timer_wsm.start_timer();
   // WorkspaceManager for internal local variables
   const Int nk_pack = ekat::npack<Spack>(m_num_levs);
   const auto policy = ekat::ExeSpaceUtils<KT::ExeSpace>::get_default_team_policy(m_num_cols, nk_pack);
   ekat::WorkspaceManager<Spack, KT::Device> workspace_mgr(m_buffer.wsm_data, nk_pack, 52, policy);
-  timer_wsm.stop_timer();
-  wsm_times.push_back(timer_wsm.report_time("wsm",get_comm()));
 
-  timer_main.start_timer();
+  timer.start_timer();
   // Run p3 main
   P3F::p3_main(prog_state, diag_inputs, diag_outputs, infrastructure,
                history_only, workspace_mgr, m_num_cols, m_num_levs);
-  timer_main.stop_timer();
-  p3_main_times.push_back(timer_main.report_time("p3_main",get_comm()));
+  timer.stop_timer();
+  p3_main_times.push_back(timer.report_time("      p3_main-time:",get_comm()));
 
   // Conduct the post-processing of the p3_main output.
   Kokkos::parallel_for(
@@ -41,9 +36,6 @@ void P3Microphysics::run_impl (const int dt)
     p3_postproc
   ); // Kokkos::parallel_for(p3_main_local_vals)
   Kokkos::fence();
-
-  timer_all.stop_timer();
-  run_impl_times.push_back(timer_all.report_time("run_impl",get_comm()));
 }
 
 } // namespace scream
